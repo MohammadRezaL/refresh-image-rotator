83a6ff187883a8a9bb4ae640f37e97ef2b7d65e2
MohammadReza Lalehgani <104711275+MohammadRezaL@users.noreply.github.com>
2026-07-24 11:40:40 +0330

    Add new image paths to ROTATOR_IMAGES array

--- images.js
+++ images.js
@@ -3,14 +3,14 @@
  *
  * Example:
  * window.ROTATOR_IMAGES = [
- *     "./images/01.svg",
-  "./images/02.svg",
-  "./images/03.svg",
  *   "./images/photo-1.jpg",
  *   "./images/photo-2.png",
  *   "./images/photo-3.webp",
  * ];
  */
 window.ROTATOR_IMAGES = [
+  "./images/01.svg",
+  "./images/02.svg",
+  "./images/03.svg",
   "./images/Leon.png",
 ];
